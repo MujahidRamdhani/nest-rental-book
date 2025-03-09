79f460f218b5248fde98cf698cc86e287e26f9f4
Mujahid <mujahidramdhani@gmail.com>
2025-03-09 09:02:30 +0700

    add table penalty

---
+++ prisma/migrations/20250309020144_delete_penalty_from_borrowing/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `penalty` on the `Borrowing` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Borrowing" DROP COLUMN "penalty";
